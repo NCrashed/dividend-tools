b7a2448c7bdee82509de59aa0ade077c9f13c7cf
NCrashed <ncrashed@gmail.com>
2021-07-09 21:51:34 +0300

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,17 @@
+with import ./nix/pkgs.nix {};
+
+stdenv.mkDerivation rec {
+  name = "rust-env";
+  env = buildEnv { name = name; paths = buildInputs; };
+
+  buildInputs = [
+    rustup
+    clang
+    llvm
+    llvmPackages.libclang
+    zola
+  ];
+  shellHook = ''
+  export LIBCLANG_PATH="${llvmPackages.libclang}/lib"
+  '';
+}
