ad08024f41e3d013853eac1b00dc1618275bd0eb
Anton Gushcha <ncrashed@protonmail.com>
2021-09-30 22:12:14 +0300

    Refactor asset loading

--- dividend-tools/static/js/global.js
+++ dividend-tools/static/js/global.js
@@ -2,40 +2,33 @@ const server = new StellarSdk.Server("https://horizon.stellar.org");
 
 const mtl_foundation = "GDX23CPGMQ4LN55VGEDVFZPAJMAUEHSHAMJ2GMCU2ZSHN5QF4TMZYPIS";
 
-async function getMtlAsset() {
+async function getAsset(code, issuer) {
   try {
     let resp = await server
       .assets()
-      .forCode("MTL")
-      .forIssuer("GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V")
+      .forCode(code)
+      .forIssuer(issuer)
       .call();
     if (resp.records.length > 0) {
       return resp.records[0];
     } else {
       console.log(resp);
-      console.error("Cannot find MTL asset!");
+      console.error("Cannot find " + code + " asset!");
     }
   } catch(err) {
     console.error(err);
-    console.error("Cannot find MTL asset!");
+    console.error("Cannot find " + code + " asset!");
   }
 }
 
+async function getMtlAsset() {
+  return getAsset("MTL", "GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V");
+}
+
+async function getMtlCityAsset() {
+  return getAsset("MTLCITY", "GDUI7JVKWZV4KJVY4EJYBXMGXC2J3ZC67Z6O5QFP4ZMVQM2U5JXK2OK3");
+}
+
 async function getEurMtlAsset() {
-  try {
-    let resp = await server
-      .assets()
-      .forCode("EURMTL")
-      .forIssuer("GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V")
-      .call();
-    if (resp.records.length > 0) {
-      return resp.records[0];
-    } else {
-      console.log(resp);
-      console.error("Cannot find EURMTL asset!");
-    }
-  } catch(err) {
-    console.error(err);
-    console.error("Cannot find EURMTL asset!");
-  }
+  return getAsset("EURMTL", "GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V");
 }
