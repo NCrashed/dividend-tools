53ef660be13cd5f65d8fcac0098b13359af677e0
attid <attid0@gmail.com>
2021-11-29 22:02:11 +0100

    auto replenish

--- docs/js/dividends.js
+++ docs/js/dividends.js
@@ -10,6 +10,27 @@ async function makeDividendsTx(amount, memo, offset, sequence, mtl, mtl_city, eu
   const account = await server.loadAccount(mtl_foundation, sequence);
   const fee = await server.fetchBaseFee();
 
+  //load list 
+  const bigOffset = offset > 0;
+  var replenishList = new Map();
+  const replenishKey = "GDEK5KGFA3WCG3F2MLSXFGLR4T4M6W6BMGWY6FBDSDQM6HXFMRSTEWBW";
+  var replenishAmount = 0.0000000;
+  $.ajaxSetup({
+      async: false
+  });
+  $.getJSON('https://raw.githubusercontent.com/montelibero-org/mtl/main/json/bodreplenish.json' ) 
+    .done(function( data ) {//
+	  //console.log(data);
+      for (key in data) {
+        if (data.hasOwnProperty(key)) {
+          console.log("key = " + key);
+          console.log("value = " + data[key]);
+          replenishList.set(key, data[key]);
+        } 
+      }
+    }) 
+  //
+
   var options = { fee, networkPassphrase: StellarSdk.Networks.PUBLIC };
   if (memo.length > 0) {
       options.memo = StellarSdk.Memo.text(memo);
@@ -23,8 +44,20 @@ async function makeDividendsTx(amount, memo, offset, sequence, mtl, mtl_city, eu
   }
   
   shares.forEach(function (a) {
-      const dividendAmount = (a.mtl_share * amount).toFixed(7);
-      // console.log("Funding", a.account_id, "with", dividendAmount);
+	  var dividendAmount = 0.00000;
+	  if (replenishList.has(a.account_id)){
+		const persent = parseFloat(replenishList.get(a.account_id)); 
+		dividendAmount = (a.mtl_share * amount).toFixed(7);
+	    var calc = parseFloat(dividendAmount * persent / 100).toFixed(7);
+		dividendAmount = parseFloat(dividendAmount - calc).toFixed(7);
+		replenishAmount = parseFloat(replenishAmount + calc).toFixed(7);
+        console.log(replenishAmount);
+        console.log("Founding", a.account_id, "with", dividendAmount,'persent ',persent);
+	  }
+	  else{
+		dividendAmount = (a.mtl_share * amount).toFixed(7);
+	  }
+      console.log("Funding", a.account_id, "with", dividendAmount);
       if (offset > 0) {
         offset = offset - 1;
       } else if (dividendAmount > 0 && a.has_eurmtl) {
@@ -46,6 +79,22 @@ async function makeDividendsTx(amount, memo, offset, sequence, mtl, mtl_city, eu
       } 
     }
   );
+  
+  // add replenishAmount
+  if (bigOffset){
+    console.log(replenishAmount);
+    builder.addOperation(
+	  StellarSdk.Operation.payment({
+		  destination: replenishKey,
+		  asset: new StellarSdk.Asset(
+			  eurmtl.asset_code,
+			  eurmtl.asset_issuer
+		    ),
+		  amount: replenishAmount.toString(),
+	  })
+    );
+  }
+  
 
   const transaction = builder
         .setTimeout(StellarSdk.TimeoutInfinite)
